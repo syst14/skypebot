e97de40c3363d0076293d28c8bba914c3cb7f2c8
syst14 <38044201+syst14@users.noreply.github.com>
2019-01-15 11:15:10 +0200

    Update bot-cli.py

--- bot-cli.py
+++ bot-cli.py
@@ -2,8 +2,6 @@ import sys
 from bothelper import BotHelper
 from main import get_token
 
-APP_ID = 'YOUR_APP_ID'
-PASSWORD = 'YOUR_PASSWORD'
 exception_msg = 'Expected arguments: "bot-cli.py message conversationID,N...'
 
 args = sys.argv
